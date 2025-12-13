b95e7169430e18f75ab226cb2d2c9f1e1ae06306
DaviLadwig <ladwigdavi@gmail.com>
2025-12-13 11:42:55 -0300

    Substituição do hero, agora conta com uma nova animação.

--- script.js
+++ script.js
@@ -81,8 +81,67 @@ cards.forEach(card => {
 });
 
 
-// FORMULARIO + MODAL
+//HERP COM ANIMAÇÃO 
 
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
 
+let particlesArray = [];
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  initParticles();
+});
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = (Math.random() - 0.5) * 0.6;
+    this.speedY = (Math.random() - 0.5) * 0.6;
+    this.opacity = Math.random() * 0.5 + 0.3;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(74, 163, 255, ${this.opacity})`;
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particlesArray = [];
+  const numberOfParticles = Math.floor((canvas.width * canvas.height) / 15000);
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particlesArray.forEach(particle => {
+    particle.update();
+    particle.draw();
+  });
+
+  requestAnimationFrame(animateParticles);
+}
+
+initParticles();
+animateParticles();
